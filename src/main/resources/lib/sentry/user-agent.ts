4c26940a5c89910c49920119928f753c57ea06cd
Tom Arild Jakobsen <tajakobsen@gmail.com>
2023-04-20 10:48:09 +0200

    Bump dependency version

--- src/main/resources/lib/sentry/user-agent.ts
+++ src/main/resources/lib/sentry/user-agent.ts
@@ -1,32 +1,29 @@
+import { Request } from "@item-enonic-types/global/controller";
 const Parser = Java.type("ua_parser.Parser");
 
-export function parseUserAgent(req: XP.Request): UserAgentData {
+export function parseUserAgent(req: Request): UserAgentData {
   const uaParser = new Parser();
   const client: UserAgentClient = uaParser.parse(req.headers["User-Agent"]);
 
   return {
     browser: {
       name: client.userAgent.family,
-      version: getVersionString([client.userAgent.major, client.userAgent.minor, client.userAgent.patch])
+      version: getVersionString([client.userAgent.major, client.userAgent.minor, client.userAgent.patch]),
     },
 
     os: {
       name: client.os.family,
-      version: getVersionString([client.os.major, client.os.minor, client.os.patch, client.os.patchMinor])
+      version: getVersionString([client.os.major, client.os.minor, client.os.patch, client.os.patchMinor]),
     },
 
     device: {
-      family: client.device.family !== "Other"
-        ? client.device.family
-        : undefined
+      family: client.device.family !== "Other" ? client.device.family : undefined,
     },
   };
 }
 
 function getVersionString(arr: Array<string | undefined>): string | null {
-  const version = arr
-    .filter((v) => v && v.length > 0)
-    .join(".")
+  const version = arr.filter((v) => v && v.length > 0).join(".");
 
   return version.length > 0 ? version : null;
 }
@@ -35,29 +32,29 @@ export interface UserAgentData {
   browser: {
     name?: string;
     version: string | null;
-  }
+  };
 
   os: {
     name?: string;
     version: string | null;
-  }
+  };
 
   device: {
     family?: string;
-  }
+  };
 }
 
 interface UserAgentClient {
   device: {
-    family?: string
-  }
+    family?: string;
+  };
 
   userAgent: {
     family?: string;
     major?: string;
     minor?: string;
     patch?: string;
-  }
+  };
 
   os: {
     family?: string;
@@ -65,5 +62,5 @@ interface UserAgentClient {
     minor?: string;
     patch?: string;
     patchMinor?: string;
-  }
+  };
 }
